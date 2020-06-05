0e7def11346e2c71cefa27e2826cf304c93ff901
SAFAME <SAFAME108@GMAIL.COM>
2020-06-05 19:01:53 +0500

    Project solution

---
+++ MartSystem/BusinessLayer/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    class Product
+    {
+        public String ProductID { get; set; }
+        public String ProductName { get; set; }
+        public String SupplierID { get; set; }
+        public String CategoryID { get; set; }
+        public String ProductDesc { get; set; }
+        public Double UnitPrice { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public int TotalQuantity { get; set; }
+    }
+}
